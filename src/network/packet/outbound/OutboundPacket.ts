172541a9076a4bba64ada1bfc0237a0a4e5e6009
samjmckenzie <25828757+samjmckenzie@users.noreply.github.com>
2018-12-22 16:58:36 +0100

    Remove comments, use BufferWriter for the getBuffer method

--- src/network/packet/outbound/OutboundPacket.ts
+++ src/network/packet/outbound/OutboundPacket.ts
@@ -1,29 +1,22 @@
 import { Packet, IPacketData } from '../Packet';
-import { IWriteBufferDescription, toBuffer, BufferDataType, BufferReader } from '../../../util/buffer';
 import { OutboundMessageType } from '../../OutboundMessageType';
+import { BufferWriter } from '../../../util/buffer';
 
 export interface IOutboundPacketData extends IPacketData {
     messageType: OutboundMessageType;
 }
 
 export abstract class OutboundPacket<DataType extends IOutboundPacketData> extends Packet {
-    // protected descriptions: IWriteBufferDescription[];
+    private bufferWriter: BufferWriter = new BufferWriter();
 
     constructor(private data: DataType) {
         super();
-        // this.descriptions = this.getDescriptions(data);
     }
 
-    // protected abstract getDescriptions(data: DataType): IWriteBufferDescription[];
-
-    // toBuffer(): Buffer {
-    //     return toBuffer(this.descriptions);
-    // }
-
-    protected abstract getBuffer(data: DataType): Buffer;
+    protected abstract getBuffer(bufferWriter: BufferWriter, data: DataType): Buffer;
 
     toBuffer(): Buffer {
-        return this.getBuffer(this.data);
+        return this.getBuffer(this.bufferWriter, this.data);
     }
 
     fromBuffer(): DataType {
