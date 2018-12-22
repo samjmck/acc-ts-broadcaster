39f02057ffcab57975ef1521325eda48bce8965a
samjmckenzie <25828757+samjmckenzie@users.noreply.github.com>
2018-12-22 16:58:12 +0100

    Use new getBuffer implementation

--- src/network/packet/outbound/RegisterCommandApplicationPacket.ts
+++ src/network/packet/outbound/RegisterCommandApplicationPacket.ts
@@ -1,5 +1,5 @@
 import { IOutboundPacketData, OutboundPacket } from './OutboundPacket';
-import { IWriteBufferDescription, BufferDataType, getStringBuffer } from '../../../util/buffer';
+import { getStringBuffer, BufferWriter } from '../../../util/buffer';
 import { OutboundMessageType } from '../../OutboundMessageType';
 
 export interface IRegisterCommandApplicationPacketData extends IOutboundPacketData {
@@ -12,29 +12,15 @@ export interface IRegisterCommandApplicationPacketData extends IOutboundPacketDa
 }
 
 export class RegisterCommandApplicationPacket extends OutboundPacket<IRegisterCommandApplicationPacketData> {
-    protected getBuffer(data: IRegisterCommandApplicationPacketData) {
-        // alloc 2 bytes for UInt8 for message type and protocol version (first byte is always message type)
-        const initialBuffer = Buffer.alloc(2);
-        // UInt8 --> unsigned 8-bit integer --> 0-255
-        // offset 0, start from start
-        initialBuffer.writeUInt8(data.messageType, 0);
-        initialBuffer.writeUInt8(data.protocolVersion, 1);
+    protected getBuffer(bufferWriter: BufferWriter, data: IRegisterCommandApplicationPacketData) {
+        bufferWriter
+            .writeUInt8(data.messageType)
+            .writeUInt8(data.protocolVersion)
+            .writeString(data.displayName)
+            .writeString(data.password)
+            .writeUInt32LE(data.updateInterval)
+            .writeString(data.commandPassword);
 
-        const displayNameBuffer = getStringBuffer(data.displayName);
-        const passwordBuffer = getStringBuffer(data.password);
-
-        // alloc 4 bytes for 32-bit (4 x 8-bits) buffer
-        const updateIntervalBuffer = Buffer.alloc(4);
-        updateIntervalBuffer.writeUInt32LE(data.updateInterval, 0);
-
-        const commandPasswordBuffer = getStringBuffer(data.commandPassword);
-
-        return Buffer.concat([
-            initialBuffer,
-            displayNameBuffer,
-            passwordBuffer,
-            updateIntervalBuffer,
-            commandPasswordBuffer,
-        ]);
+        return bufferWriter.buffer;
     }
 }
